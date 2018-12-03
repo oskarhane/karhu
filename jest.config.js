b7a8b8db0b6c6fcb433853f1c628aa905d3f994a
Oskar Hane <oh@oskarhane.com>
2018-12-03 08:18:38 +0100

    Lower test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   coverageReporters: ['text', 'clover'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
     },
   },
   globals: {
