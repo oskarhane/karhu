54af9c56b8618644e5b3efad0a0883ae2c725c63
Oskar Hane <oh@oskarhane.com>
2018-12-03 08:08:34 +0100

    Upgrade typescript

--- packages/karhu/src/types.ts
+++ packages/karhu/src/types.ts
@@ -1,15 +1,15 @@
-export type UnregisteredCommand = {
+export interface UnregisteredCommand {
   id?: string;
   name: string;
   contexts?: string[];
   keywords: string[];
   actions: ActionsObject;
   render: (c: Command) => JSX.Element | string;
-};
+}
 
-export type CommandMetadata = {
+export interface CommandMetadata {
   calls: number;
-};
+}
 
 export interface Command extends UnregisteredCommand {
   id: string;
@@ -22,23 +22,23 @@ export enum MatchClass {
   STARTS = 3,
   CONTAINS = 2,
   ACRONYM = 1,
-  NO = 0,
+  NO = 0
 }
 
-export type ClassifiedMatch = {
+export interface ClassifiedMatch {
   id: string;
   score: MatchClass;
-};
+}
 
 export type ClassifiedMatches = ClassifiedMatch[];
 
 export type EntryGraph = any;
-export type EntryGraphRecord = {
+export interface EntryGraphRecord {
   id: string;
   calls: number;
-};
+}
 
-export type ActionsObject = {
+export interface ActionsObject {
   onExec: () => void;
   onShow?: (id: string) => void;
-};
+}
