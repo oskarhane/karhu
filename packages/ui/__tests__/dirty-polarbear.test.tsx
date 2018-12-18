fceb0f1a6e69119d0268665b15aac398b1840648
Oskar Hane <oh@oskarhane.com>
2018-12-18 16:08:41 +0100

    Extract list interactions to separate component

--- packages/ui/__tests__/dirty-polarbear.test.tsx
+++ packages/ui/__tests__/dirty-polarbear.test.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import Karhu from '@karhu/core';
-import { UnregisteredCommand, Command } from '@karhu/core/lib/types';
-import { KarhuProvider } from '@karhu/react';
-
 import { DirtyPolarBear } from '../src';
 
 let portalRoot = document.getElementById('portal');
@@ -51,108 +48,5 @@ describe('polarbear', () => {
 
     // Then
     expect(baseElement.querySelector('.karhu')).toBeNull();
-  });
-  test('lists and interacts with the commands in the list', () => {
-    jest.useFakeTimers();
-
-    const cmd1: UnregisteredCommand = {
-      id: 'test1',
-      name: 'Test Command 1',
-      keywords: ['test'],
-      render: jest.fn((c: Command) => {
-        return <div>{c.name}</div>;
-      }),
-      actions: {
-        onExec: jest.fn(),
-      },
-    };
-    karhu.addCommand(cmd1);
-    const cmd2: UnregisteredCommand = {
-      id: 'test2',
-      name: 'Test Command 2',
-      keywords: ['test'],
-      render: jest.fn((c: Command) => {
-        return <div>{c.name}</div>;
-      }),
-      actions: {
-        onExec: jest.fn(),
-      },
-    };
-    karhu.addCommand(cmd2);
-    const cmd3: UnregisteredCommand = {
-      id: 'test3',
-      name: 'Test Command 3',
-      keywords: ['test'],
-      render: jest.fn((c: Command) => {
-        return <div>{c.name}</div>;
-      }),
-      actions: {
-        onExec: jest.fn(),
-      },
-    };
-    karhu.addCommand(cmd3);
-
-    const resultEntryGraph = jest.fn();
-
-    // When
-    const { baseElement, getByText } = render(
-      <KarhuProvider value={karhu}>
-        <DirtyPolarBear openWith={openWith} closeWith={closeWith} element={portalRoot} onExec={resultEntryGraph} />
-      </KarhuProvider>,
-    );
-
-    // When activating the bar
-    fireEvent.keyDown(document.body, {
-      keyCode: 75, // k
-    });
-
-    const input = baseElement.querySelector('input') as HTMLInputElement;
-
-    // Typing a character
-    fireEvent.change(input, {
-      target: {
-        value: 't', // t
-      },
-    });
-
-    // Then
-    expect(getByText(cmd1.name)).not.toBeNull();
-    expect(getByText(cmd2.name)).not.toBeNull();
-    expect(getByText(cmd3.name)).not.toBeNull();
-    let active = baseElement.querySelector('.active') as HTMLElement;
-    expect(active.textContent).toEqual(cmd1.name);
-
-    // Arrow up should move to last
-    fireEvent.keyDown(document.body, {
-      keyCode: 38, // ^
-    });
-
-    // Then
-    active = baseElement.querySelector('.active') as HTMLElement;
-    expect(active.textContent).toEqual(cmd3.name);
-
-    // When arrow up again
-    fireEvent.keyDown(document.body, {
-      keyCode: 38, // ^
-    });
-
-    // Then
-    active = baseElement.querySelector('.active') as HTMLElement;
-    expect(active.textContent).toEqual(cmd2.name);
-
-    // Enter to execute
-    fireEvent.keyDown(document.body, {
-      keyCode: 13, // enter
-    });
-
-    // Then
-    jest.runOnlyPendingTimers();
-
-    expect(baseElement.querySelector('.karhu')).toBeNull();
-    expect(cmd1.actions.onExec).not.toHaveBeenCalled();
-    expect(cmd2.actions.onExec).toHaveBeenCalled();
-    expect(cmd3.actions.onExec).not.toHaveBeenCalled();
-    expect(resultEntryGraph).toHaveBeenCalledTimes(1);
-    expect(resultEntryGraph).toHaveBeenCalledWith({ next: { t: { commands: [{ calls: 1, id: 'test2' }] } } });
   });
 });
--- packages/ui/src/dirty-polarbear/CommandList.tsx
+++ packages/ui/src/dirty-polarbear/CommandList.tsx
@@ -1,117 +1,40 @@
 import React from 'react';
-import {Command} from '@karhu/core/lib/types';
+import { Command } from '@karhu/core/lib/types';
+import ListSelector from '../ListSelector';
 
 type Props = {
-    commands: Command[];
-    onExec: (id: string) => void;
+  commands: Command[];
+  onExec: (id: string) => void;
 };
 type State = {
-    activeCommand?: string;
-    hadManualSelection: boolean;
+  activeCommand?: string;
+  hadManualSelection: boolean;
 };
 
 export default class CommandList extends React.Component<Props, State> {
-    state = {
-        activeCommand: undefined,
-        hadManualSelection: false
-    };
-    componentDidMount() {
-        this.updateActiveCommand();
-        window.addEventListener('keydown', this.handleKeyPress);
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyPress);
-    }
-    componentDidUpdate() {
-        this.updateActiveCommand();
-    }
-    updateActiveCommand() {
-        // Nothing selected and no list => Do nothing
-        // Reset hadManualSelection to start selecting first result on new results
-        if (!this.props.commands.length && this.state.activeCommand === undefined) {
-            // Reset manual selection
-            if (this.state.hadManualSelection) {
-                this.setState({hadManualSelection: false});
-            }
-            return;
-        }
-        // No commands in list
-        if (!this.props.commands.length) {
-            this.setState({activeCommand: undefined});
-            return;
-        }
-        // Nothing selected, but we have result => preselect first
-        if (this.state.activeCommand === undefined) {
-            this.setState({activeCommand: this.props.commands[0].id});
-            return;
-        }
-
-        // No manual selection done => Keep first selected
-        if (this.state.hadManualSelection === false && this.state.activeCommand !== this.props.commands[0].id) {
-            this.setState({activeCommand: this.props.commands[0].id});
-            return;
-        }
-
-        // Selected item not in list anymore
-        if (this.props.commands.map(c => c.id).indexOf(this.state.activeCommand || '') < 0) {
-            this.setState({activeCommand: undefined});
-        }
-    }
-    handleKeyPress = (e: any) => {
-        const {keyCode} = e;
-        // Arrows up and down
-        if ([38, 40].indexOf(keyCode) >= 0) {
-            e.preventDefault();
-            const commandIds: string[] = this.props.commands.map(c => c.id);
-            const currentIndex: number = commandIds.indexOf(this.state.activeCommand || '');
-            let nextActiveCommand: string = commandIds[0];
-
-            if (currentIndex <= 0 && keyCode === 38) {
-                // Nothing (or first item) selected and go up = start form bottom
-                nextActiveCommand = commandIds[commandIds.length - 1];
-            } else if (currentIndex < 0 && keyCode === 40) {
-                // Nothing selected and go down = start form top
-                nextActiveCommand = commandIds[0];
-            } else if (currentIndex > 0 && keyCode === 38) {
-                // Anything but the first selected and go up = go up
-                nextActiveCommand = commandIds[currentIndex - 1];
-            } else if (currentIndex < commandIds.length - 1 && keyCode === 40) {
-                // Anything but the last selected and go down = go down
-                nextActiveCommand = commandIds[currentIndex + 1];
-            } else if (currentIndex >= commandIds.length - 1 && keyCode === 40) {
-                // Last item selected and go down = start from start
-                nextActiveCommand = commandIds[0];
-            }
-            this.setState({activeCommand: nextActiveCommand, hadManualSelection: true});
-            return;
-        }
-        // Enter
-        if (keyCode === 13) {
-            e.preventDefault();
-            if (this.state.activeCommand === undefined) {
-                return;
-            }
-            this.props.onExec(this.state.activeCommand || '');
-        }
-    };
-    setActiveCommand = (id: string) => {
-        this.setState({activeCommand: id, hadManualSelection: true});
-    };
-    render() {
-        const {commands, onExec} = this.props;
-        return (
-            <ul>
+  render() {
+    const { commands, onExec } = this.props;
+    return (
+      <ul>
+        <ListSelector commands={commands} onExec={onExec}>
+          {({ onSelect, activeCommandId, onExec }) => {
+            return (
+              <React.Fragment>
                 {commands.map((c: Command) => (
-                    <li
-                        className={this.state.activeCommand === c.id ? 'active' : ''}
-                        onMouseEnter={() => this.setActiveCommand(c.id)}
-                        onClick={() => onExec(c.id)}
-                        key={c.id}
-                    >
-                        {c.render(c)}
-                    </li>
+                  <li
+                    className={activeCommandId === c.id ? 'active' : ''}
+                    onMouseEnter={() => onSelect(c.id)}
+                    onClick={() => onExec(c.id)}
+                    key={c.id}
+                  >
+                    {c.render(c)}
+                  </li>
                 ))}
-            </ul>
-        );
-    }
+              </React.Fragment>
+            );
+          }}
+        </ListSelector>
+      </ul>
+    );
+  }
 }
